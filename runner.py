f7bfcdf57fdf1dba512d57541bcc6e98c3171a00
Quoc Thanh <thanhboks128@gmail.com>
2024-10-19 22:36:58 +0700

    fix link img

--- runner.py
+++ runner.py
@@ -19,7 +19,7 @@ size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
 
 # Fonts
-OPEN_SANS = "assets/fonts/OpenSans-Regular.ttf"
+OPEN_SANS = "C:\code\Python\minesweeper-bot\\assets\\fonts\OpenSans-Regular.ttf"
 smallFont = pygame.font.Font(OPEN_SANS, 20)
 mediumFont = pygame.font.Font(OPEN_SANS, 28)
 largeFont = pygame.font.Font(OPEN_SANS, 40)
@@ -32,9 +32,9 @@ cell_size = int(min(board_width / WIDTH, board_height / HEIGHT))
 board_origin = (BOARD_PADDING, BOARD_PADDING)
 
 # Add images
-flag = pygame.image.load("assets/images/flag.png")
+flag = pygame.image.load("C:\code\Python\minesweeper-bot\\assets\images\\flag.png")
 flag = pygame.transform.scale(flag, (cell_size, cell_size))
-mine = pygame.image.load("assets/images/mine.png")
+mine = pygame.image.load("C:\code\Python\minesweeper-bot\\assets\images\mine.png")
 mine = pygame.transform.scale(mine, (cell_size, cell_size))
 
 # Create game and AI agent
@@ -161,7 +161,7 @@ while True:
 
     move = None
 
-    left, _, right = pygame.mouse.get_pressedh()
+    left, _, right = pygame.mouse.get_pressed()
 
     # Check for a right-click to toggle flagging
     if right == 1 and not lost:
