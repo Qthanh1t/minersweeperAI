66473233343c19d0b41574f9a475ecf10d6703e0
Quoc Thanh <thanhboks128@gmail.com>
2024-12-07 21:05:25 +0700

    add comment

--- minesweeper.py
+++ minesweeper.py
@@ -4,18 +4,16 @@ import random
 
 
 class Minesweeper():
-    """
-    Minesweeper game representation
-    """
+
 
     def __init__(self, height=8, width=8, mines=8):
 
-        # Set initial width, height, and number of mines
+        # Set chiều cao, chiều rộng và số mìn
         self.height = height
         self.width = width
         self.mines = set()
 
-        # Initialize an empty field with no mines
+        #khởi tạo
         self.board = []
         for i in range(self.height):
             row = []
@@ -23,7 +21,7 @@ class Minesweeper():
                 row.append(False)
             self.board.append(row)
 
-        # Add mines randomly
+        # random mìn
         while len(self.mines) != mines:
             i = random.randrange(height)
             j = random.randrange(width)
@@ -31,13 +29,12 @@ class Minesweeper():
                 self.mines.add((i, j))
                 self.board[i][j] = True
 
-        # At first, player has found no mines
+        # mìn đã tìm được
         self.mines_found = set()
 
     def print(self):
         """
-        Prints a text-based representation
-        of where mines are located.
+        kiểm tra vị trị mìn
         """
         for i in range(self.height):
             print("--" * self.width + "-")
@@ -55,23 +52,21 @@ class Minesweeper():
 
     def nearby_mines(self, cell):
         """
-        Returns the number of mines that are
-        within one row and column of a given cell,
-        not including the cell itself.
+        Số lượng mìn ở gần
         """
 
-        # Keep count of nearby mines
+        
         count = 0
 
-        # Loop over all cells within one row and column
+        # vòng lặp các ô ở gần ô cần kiểm tra
         for i in range(cell[0] - 1, cell[0] + 2):
             for j in range(cell[1] - 1, cell[1] + 2):
 
-                # Ignore the cell itself
+                # bỏ qua ô đang kiểm tra 
                 if (i, j) == cell:
                     continue
 
-                # Update count if cell in bounds and is mine
+                # cập nhật nếu tìm thấy mìn
                 if 0 <= i < self.height and 0 <= j < self.width:
                     if self.board[i][j]:
                         count += 1
@@ -80,16 +75,16 @@ class Minesweeper():
 
     def won(self):
         """
-        Checks if all mines have been flagged.
+        Kiểm tra nếu tất cả mìn đã được đánh dấu
         """
         return self.mines_found == self.mines
 
 
 class Sentence():
     """
-    Logical statement about a Minesweeper game
-    A sentence consists of a set of board cells,
-    and a count of the number of those cells which are mines.
+    Các thông tin về màn chơi
+    Mỗi câu bao gồm một tập hợp các ô bảng,
+    và số lượng của các ô là mìn.
     """
 
     def __init__(self, cells, count):
@@ -104,22 +99,21 @@ class Sentence():
 
     def known_mines(self):
         """
-        Returns the set of all cells in self.cells known to be mines.
+        Trả về tập hợp tất cả các ô trong self.cells đã biết là mìn.
         """
         if len(self.cells) == self.count:
             return self.cells
 
     def known_safes(self):
         """
-        Returns the set of all cells in self.cells known to be safe.
+        Trả về tập hợp tất cả các ô trong self.cells đã biết an toàn.
         """
         if self.count == 0:
             return self.cells
 
     def mark_mine(self, cell):
         """
-        Updates internal knowledge representation given the fact that
-        a cell is known to be a mine.
+        Cập nhật tri thức khi biết một ô là mìn
         """
         if cell in self.cells:
             self.cells.discard(cell)
@@ -127,8 +121,7 @@ class Sentence():
 
     def mark_safe(self, cell):
         """
-        Updates internal knowledge representation given the fact that
-        a cell is known to be safe.
+        Cập nhật tri thức khi biết một ô an toàn
         """
         if cell in self.cells:
             self.cells.discard(cell)
@@ -136,29 +129,28 @@ class Sentence():
 
 class MinesweeperAI():
     """
-    Minesweeper game player
+    Minesweeper AI
     """
 
     def __init__(self, height=8, width=8):
 
-        # Set initial height and width
+        # Set chiều cao và chiều rộng
         self.height = height
         self.width = width
 
-        # Keep track of which cells have been clicked on
+        # Theo dõi các ô nào đã được mở
         self.moves_made = set()
 
-        # Keep track of cells known to be safe or mines
+        # Theo dõi các ô được biết là an toàn hoặc mìn
         self.mines = set()
         self.safes = set()
 
-        # List of sentences about the game known to be true
+        # Danh sách thông tin về trò chơi được biết là đúng
         self.knowledge = []
 
     def mark_mine(self, cell):
         """
-        Marks a cell as a mine, and updates all knowledge
-        to mark that cell as a mine as well.
+        Đánh dấu một ô là mìn và cập nhật tất cả các kiến thức
         """
         self.mines.add(cell)
         for sentence in self.knowledge:
@@ -166,8 +158,7 @@ class MinesweeperAI():
 
     def mark_safe(self, cell):
         """
-        Marks a cell as safe, and updates all knowledge
-        to mark that cell as safe as well.
+        Đánh dấu một ô là an toàn và cập nhật tất cả các kiến thức
         """
         self.safes.add(cell)
         for sentence in self.knowledge:
@@ -189,30 +180,29 @@ class MinesweeperAI():
 
     def add_knowledge(self, cell, count):
         """
-        Called when the Minesweeper board tells us, for a given
-        safe cell, how many neighboring cells have mines in them.
-        This function should:
-            1) mark the cell as a move that has been made
-            2) mark the cell as safe
-            3) add a new sentence to the AI's knowledge base
-               based on the value of `cell` and `count`
-            4) mark any additional cells as safe or as mines
-               if it can be concluded based on the AI's knowledge base
-            5) add any new sentences to the AI's knowledge base
-               if they can be inferred from existing knowledge
+        Được gọi khi một ô được mở và kiểm tra xem có bao nhiêu mìn xung quanh
+        Chức năng:
+        1) Đánh dấu ô là đã mở
+        2) Đánh dấu ô là an toàn
+        3) Thêm một câu mới vào cơ sở kiến thức của AI
+        dựa trên giá trị của `cell` và` Count`
+        4) Đánh dấu bất kỳ ô bổ sung nào là an toàn hoặc là mỏ
+        Nếu nó có thể được kết luận dựa trên cơ sở kiến thức của AI
+        5) Thêm bất kỳ câu mới nào vào cơ sở kiến thức của AI
+        Nếu chúng có thể được suy ra từ kiến thức hiện có
         """
 
-        # Mark cell as a move that has been made
+        # đánh dấu ô đã mở
         self.moves_made.add(cell)
 
-        # Mark cell as safe
+        # đánh dấu ô an toàn
         if cell not in self.safes:
             self.mark_safe(cell)
 
-        # Get nearby cells
+        # số ô ở gần
         nearby = self.nearby_cells(cell)
 
-        # Remove safe cells and moves made
+        # bỏ đi những ô đã được mở và những ô an toàn
         nearby -= self.safes | self.moves_made
 
         new_sentence = Sentence(nearby, count)
@@ -263,11 +253,10 @@ class MinesweeperAI():
 
     def make_safe_move(self):
         """
-        Returns a safe cell to choose on the Minesweeper board.
-        The move must be known to be safe, and not already a move
-        that has been made.
-        This function may use the knowledge in self.mines, self.safes
-        and self.moves_made, but should not modify any of those values.
+        Trả về một ô an toàn để chọn trên bảng.
+        Việc di chuyển phải được biết là an toàn và không phải ô đã mở
+        Hàm này có thể sử dụng kiến thức trong self.mines, self.safes
+        và self.moves_made, nhưng không được sửa đổi bất kỳ giá trị nào trong số đó.
         """
 
         safe_moves = self.safes.copy()
@@ -281,17 +270,17 @@ class MinesweeperAI():
 
     def make_random_move(self):
         """
-        Returns a move to make on the Minesweeper board.
-        Should choose randomly among cells that:
-            1) have not already been chosen, and
-            2) are not known to be mines
+        Trả lại một nước đi random
+        chọn các ô có các yếu tố sau:
+            1) chưa được mở
+            2) chưa biết đó là mìn
         """
 
         def get_random_cell():
             return (random.randrange(self.height),
                     random.randrange(self.height))
 
-        # If no possible random move, return None
+        # Nếu không có nước đi random, trả về None
         # width=8 * height=8 - mines=8 -> 56
         if len(self.moves_made) == 56:
             return None
